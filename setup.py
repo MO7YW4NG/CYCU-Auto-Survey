bc1ed2a74b7fcafeaf94a0a8156d83f829506364
MO7YW4NG <motywang@gmail.com>
2024-12-12 00:12:01 +0800

    Update .gitignore and increment version to 1.1 in setup.py

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ target = Executable(
 
 setup(
     name="CYCU-Auto-Survey",
-    version="1.0",
+    version="1.1",
     description="cycu-auto-survey-app",
     author="MO7YW4NG",
     options={'bdist_msi': {'initial_target_dir': r'[DesktopFolder]\\CYCU-Auto-Survey'},'bdist_mac': {'initial_target_dir': r'[DesktopFolder]\\CYCU-Auto-Survey'}},
